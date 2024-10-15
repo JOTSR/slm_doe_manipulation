f582dcea664b6b572de84cedb9f6ffe2c064b630
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-15 11:06:22 +0200

    feat(core): add built-in common patterns

---
+++ src/common_patterns.ts
@@ -0,0 +1,23 @@
+import type { Pattern } from '../types.ts'
+
+export function circle(
+	ceil: number,
+	offsetX: number,
+	offsetY: number,
+): Pattern {
+	return (x, y) => 255 * Number(Math.hypot(x - offsetX, y - offsetY) > ceil)
+}
+
+export function rect(
+	xMin: number,
+	xMax: number,
+	yMin: number,
+	yMax: number,
+	invert = false,
+): Pattern {
+	if (invert) {
+		return (x, y) => 255 * Number(x > xMin && x < xMax && y > yMin && y < yMax)
+	}
+
+	return (x, y) => 255 * Number(!(x > xMin && x < xMax && y > yMin && y < yMax))
+}
