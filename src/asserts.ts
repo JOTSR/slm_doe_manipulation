34b5c9f0d058a4ce1a6af70461359ce8ed928823
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-21 23:14:30 +0200

    doc(core): add jsdoc to `asserts.ts`

--- src/asserts.ts
+++ src/asserts.ts
@@ -1,3 +1,22 @@
+/**
+ * Test if a value is a positive integer (u64).
+ *
+ * value - Number to test.
+ * options - Name of the tested variable in the error message.
+ *
+ * @throws {RangeError} Number must be a positive interger.
+ *
+ * @example Usage
+ * ```ts
+ * assertIsPositiveInteger(3) //ok
+ * try {
+ * 	const myFloat = 1.2
+ * 	assertIsPositiveInteger(myFloat, { name: 'myFloat' })
+ * } catch {
+ * 	//RangeError: "myFloat": 1.2 is not a positive interger
+ * }
+ * ```
+ */
 export function assertIsPositiveInteger(
 	value: number,
 	{ name }: { name: string } = { name: 'value' },
@@ -7,21 +26,40 @@ export function assertIsPositiveInteger(
 	}
 }
 
+/**
+ * Test is a value is included between to bounds.
+ *
+ * value - Value that must be inside bounds.
+ * bounds - Specified bounds.
+ * options - Name of the tested variable in the error message and bounds exlusive behaviour.
+ *
+ * @throws {RangeError} Number must be included in specied bounds.
+ *
+ * @example Usage
+ * ```ts
+ * assertIsBetween(0.3, [0.1, 0.3]) //ok
+ * try {
+ * 	assertIsBetween(0.3, [0.1, 0.3], { name: 'bounded', exclusive: [false, true] })
+ * } catch {
+ * 	//RangeError: "bounded": 0.3 must be >= 0.1 and < 0.3
+ * }
+ * ```
+ */
 export function assertIsBetween(
 	value: number,
-	range: [min: number, max: number],
+	bounds: [min: number, max: number],
 	{ name, exclusive }: {
 		name: string
 		exclusive: [boolean, boolean]
 	} = { name: 'value', exclusive: [false, false] },
 ): void {
-	const leftBound = exclusive[0] ? value <= range[0] : value < range[0]
-	const rightBound = exclusive[1] ? value <= range[1] : value < range[1]
+	const leftBound = exclusive[0] ? value <= bounds[0] : value < bounds[0]
+	const rightBound = exclusive[1] ? value <= bounds[1] : value < bounds[1]
 	if (leftBound || rightBound) {
 		throw new RangeError(
 			`"${name}: ${value}" must be ${exclusive[0] ? '>' : '>='} ${
-				range[0]
-			} and ${exclusive[1] ? '<' : '<='} ${range[1]}`,
+				bounds[0]
+			} and ${exclusive[1] ? '<' : '<='} ${bounds[1]}`,
 		)
 	}
 }
