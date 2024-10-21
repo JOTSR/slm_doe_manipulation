e03acd0d8703cf160e7d43cd3784a0c64cabfef6
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-21 23:01:31 +0200

    doc(core): add js doc to `assertEquals`

--- src/asserts.ts
+++ src/asserts.ts
@@ -26,6 +26,26 @@ export function assertIsBetween(
 	}
 }
 
+/**
+ * Compare two numbers and throw an error if there is no equality.
+ *
+ * valueA - First number to test.
+ * valueB - Second number to test.
+ * options - Name of the tested variable in the error message.
+ *
+ * @throws {RangeError} Number must be equals.
+ *
+ * @example Usage
+ * ```ts
+ * assertEquals(0.3, 0.1 + 0.2) //ok
+ * try {
+ * 	const myConstant = 1
+ * 	assertEquals(myConstant, 2, { name: 'myConstant' })
+ * } catch (e) {
+ * 	console.error(e)
+ * }
+ * ```
+ */
 export function assertEquals(
 	valueA: number,
 	valueB: number,
