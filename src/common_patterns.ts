4002365355d8b804a2a1e4e35e55cd08300b29f5
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-21 23:33:25 +0200

    doc(core): add jsdoc for `common_patterns.ts`

--- src/common_patterns.ts
+++ src/common_patterns.ts
@@ -1,13 +1,50 @@
 import type { Pattern } from '../types.ts'
 
+/**
+ * Provide a pattern that draw a b&w circle.
+ *
+ * @param radius - Radius of the circle.
+ * @param offsetX - Vertical origin.
+ * @param offsetY - Horizontal origin.
+ *
+ * @example Usage
+ * ```ts
+ * const screenOrigin = { x: 255, y: 255 }
+ * const smallBall = circle(
+ * 	25,
+ * 	screenOrigin.x,
+ * 	screenOrigin.y,
+ * )
+ * ```
+ */
 export function circle(
-	ceil: number,
+	radius: number,
 	offsetX: number,
 	offsetY: number,
 ): Pattern {
-	return (x, y) => 255 * Number(Math.hypot(x - offsetX, y - offsetY) > ceil)
+	return (x, y) => 255 * Number(Math.hypot(x - offsetX, y - offsetY) > radius)
 }
 
+/**
+ * Provide a pattern that draw a b&w rectangle.
+ *
+ * @param xMin - Rectangle vertical origin.
+ * @param xMax - Rectangle vertical end.
+ * @param yMin - Rectangle horizontal origin.
+ * @param yMax - Rectangle horizontal end.
+ * @param [invert=false] - Invert rectangle inside and outside.
+ *
+ * @example Usage
+ * ```ts
+ * const screenOrigin = { x: 255, y: 255 }
+ * const smallBox = rect(
+ * 	screeOrigin.x - 25,
+ * 	screenOrigin.x + 25,
+ * 	screenOrigin.y - 25,
+ * 	screenOrigin.y + 25,
+ * )
+ * ```
+ */
 export function rect(
 	xMin: number,
 	xMax: number,
@@ -22,6 +59,18 @@ export function rect(
 	return (x, y) => 255 * Number(!(x > xMin && x < xMax && y > yMin && y < yMax))
 }
 
+/**
+ * Provide a pattern of uniformly distributed random pixels.
+ *
+ * @param [min=0] - Minimum random value.
+ * @param [max=255] - Maximum random value.
+ * @return Pattern filled with random values.
+ *
+ * @example Usage
+ * ```ts
+ * const grayishPattern = random()
+ * ```
+ */
 export function random(min = 0, max = 255): Pattern {
 	return () => (max - min) * Math.random() + min
 }
