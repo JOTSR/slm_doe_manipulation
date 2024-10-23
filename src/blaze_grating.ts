4ae6626a81a370675e3d5e73fb676341cf8e681e
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-24 01:02:50 +0200

    feat(core): all to use tilted blaze grating

--- src/blaze_grating.ts
+++ src/blaze_grating.ts
@@ -6,18 +6,21 @@ import { Grating } from './grating.ts'
  *
  * @property max - Max height of the grating between 0 and 255 (as 0 and 2π).
  * @property count - Number of vertical blades of the grating.
+ * @property tilt - Trigonometric tilt angle to apply to the blaze in radian from vertical axis.
  *
  * @example Example
  * ```ts
  * const blaze = {
  * 	max: 255 // i.e 0-2π in phase space.
  * 	count: 25 // i.e approximatively 10px width on a 256 pixel width screen.
+ * 	tilt: Math.PI / 2 // i.e 45deg blaze grating.
  * }
  * ```
  */
 export type Blaze = {
 	max: number
 	count: number
+	tilt: number
 }
 
 /**
@@ -30,7 +33,7 @@ export type Blaze = {
  *
  * @example Usage
  * ```ts
- * const grating = blazeGrating(256, 256, { count: 25, height: 255 })
+ * const grating = blazeGrating(256, 256, { count: 25, height: 255, tilt: 0 })
  * ```
  */
 export function blazeGrating<Width extends number, Height extends number>(
@@ -52,19 +55,24 @@ export function blazeGrating<Width extends number, Height extends number>(
 	}
 
 	const grating = new Grating(width, height)
-	const pixels = grating.rawPixels
 
-	const slope = blaze.count * (blaze.max / 255)
-	const clamp = width / blaze.count
+	// 0.5 and 2 to compense x and y blaze
+	const slope = 0.5 * blaze.count * (blaze.max / 255)
+	const clamp = 2 * width / blaze.count
 
-	for (let i = 0; i < width * height * 4; i += 4) {
-		const x = (i / 4) % width
+	const cos = Math.cos(Math.PI / 4 - blaze.tilt)
+	const sin = Math.sin(Math.PI / 4 - blaze.tilt)
 
-		const brightness = slope * (x % clamp)
-		pixels[i] = brightness //red
-		pixels[i + 1] = brightness //blue
-		pixels[i + 2] = brightness //blue
-		pixels[i + 3] = 255 //alpha
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const x_ = x * cos - y * sin
+			const y_ = x * sin + y * cos
+
+			const brightnessX = slope * (x_ % clamp)
+			const brightnessY = slope * (y_ % clamp)
+			const brightness = brightnessX + brightnessY
+			grating.setPixelMono(x, y, brightness)
+		}
 	}
 
 	return grating
