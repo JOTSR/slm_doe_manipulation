8931dbd5802580f42106a4610861539d334eacc5
Julien Oculi <xdrz.jot.sr@gmail.com>
2024-10-25 13:46:38 +0200

    style(gui): deno fmt

--- gui/dev.ts
+++ gui/dev.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
-import { Builder } from "fresh/dev";
-import { app } from "./main.ts";
+import { Builder } from 'fresh/dev'
+import { app } from './main.ts'
 
-const builder = new Builder();
+const builder = new Builder()
 
-if (Deno.args.includes("build")) {
-  await builder.build(app);
+if (Deno.args.includes('build')) {
+	await builder.build(app)
 } else {
-  await builder.listen(app);
+	await builder.listen(app)
 }
